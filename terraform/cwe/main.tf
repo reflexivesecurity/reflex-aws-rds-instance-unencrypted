ceb0a6226e0f18fa09f4adaefc7678d708e36292
Max Anderson <maxwell.c.anderson@gmail.com>
2020-05-17 11:30:22 -0700

    Multi-region update

---
+++ terraform/cwe/main.tf
@@ -0,0 +1,25 @@
+module "cwe" {
+  source      = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/cwe_lambda?ref=v0.6.0"
+  name        = "RDSInstanceUnencrypted"
+  description = "Rule to detect with an RDS instance is created unencrypted."
+
+  event_pattern = <<PATTERN
+{
+  "source": [
+    "aws.rds"
+  ],
+  "detail-type": [
+    "AWS API Call via CloudTrail"
+  ],
+  "detail": {
+    "eventSource": [
+      "rds.amazonaws.com"
+    ],
+    "eventName": [
+      "CreateDBInstance"
+    ]
+  }
+}
+PATTERN
+
+}
